4a64fd6dadcac6f48898de819a6e2ec5f90cdf77
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2018-01-09 17:21:13 +0100

    "Hjælp" siden er lidt mindre basic nu. indeholder noget FAQ og kontaktinformationer på CI

--- app/src/main/java/com/lasse/bluetoothconnection/fragments/HelpFragment.java
+++ app/src/main/java/com/lasse/bluetoothconnection/fragments/HelpFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.lasse.bluetoothconnection.R;
@@ -19,6 +20,9 @@ public class HelpFragment extends Fragment {
 
         WebView webview = root.findViewById(R.id.webview);
         webview.loadUrl("file:///android_asset/help_web/index.html");
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
 
         return root;
     }
