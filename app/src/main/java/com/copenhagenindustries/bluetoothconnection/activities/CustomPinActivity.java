3a79c067aad6688e0936579148aa947f504c2957
dukocuk <duran.kse@gmail.com>
2018-01-13 00:21:26 +0100

    new updates

--- app/src/main/java/com/copenhagenindustries/bluetoothconnection/activities/CustomPinActivity.java
+++ app/src/main/java/com/copenhagenindustries/bluetoothconnection/activities/CustomPinActivity.java
@@ -3,13 +3,21 @@ package com.copenhagenindustries.bluetoothconnection.activities;
 
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.copenhagenindustries.bluetoothconnection.R;
 import com.github.omadahealth.lollipin.lib.managers.AppLock;
 import com.github.omadahealth.lollipin.lib.managers.AppLockActivity;
 import com.github.omadahealth.lollipin.lib.managers.LockManager;
+
+import uk.me.lewisdeane.ldialogs.BaseDialog;
+import uk.me.lewisdeane.ldialogs.CustomDialog;
 
 import static android.content.ContentValues.TAG;
 
@@ -25,6 +33,51 @@ public class CustomPinActivity extends AppLockActivity {
     /*
     write forget popup here
     */
+
+        Resources res = getResources();
+        // Create the builder with required paramaters - Context, Title, Positive Text
+        CustomDialog.Builder builder = new CustomDialog.Builder(this,
+                "Skift din PIN-kode",
+                "Ja");
+        builder.content("Skift din PIN-kode");
+        builder.negativeText("Nej");
+
+        //Set theme
+        builder.darkTheme(false);
+        builder.typeface(Typeface.SANS_SERIF);
+        builder.positiveColor(res.getColor(R.color.light_blue_500)); // int res, or int colorRes parameter versions available as well.
+        builder.negativeColor(res.getColor(R.color.light_blue_500));
+        builder.rightToLeft(false); // Enables right to left positioning for languages that may require so.
+        builder.titleAlignment(BaseDialog.Alignment.CENTER);
+        builder.buttonAlignment(BaseDialog.Alignment.CENTER);
+        //builder.setButtonStacking(false);
+
+
+        //Set text sizes
+        builder.titleTextSize((int) 22);
+        builder.contentTextSize((int) 22);
+        //builder.positiveButtonTextSize((int) res.getDimension(R.dimen.activity_dialog_positive_button_size));
+        //builder.negativeButtonTextSize((int) res.getDimension(R.dimen.activity_dialog_negative_button_size));
+
+        //Build the dialog.
+        CustomDialog customDialog = builder.build();
+        customDialog.setCanceledOnTouchOutside(false);
+        customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        customDialog.setClickListener(new CustomDialog.ClickListener() {
+            @Override
+            public void onConfirmClick() {
+                Toast.makeText(getApplicationContext(), "Yes", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelClick() {
+                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Show the dialog.
+        customDialog.show();
+
     }
 
     @Override
