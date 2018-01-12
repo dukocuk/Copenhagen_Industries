660a69db654213aa512fa0669091fe935b67268d
dukocuk <duran.kse@gmail.com>
2018-01-12 16:45:55 +0100

    new updates

--- app/src/main/java/com/copenhagenindustries/bluetoothconnection/fragments/SettingsFragment.java
+++ app/src/main/java/com/copenhagenindustries/bluetoothconnection/fragments/SettingsFragment.java
@@ -1,15 +1,24 @@
 package com.copenhagenindustries.bluetoothconnection.fragments;
 
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v7.preference.ListPreference;
 
 
 import com.copenhagenindustries.bluetoothconnection.R;
 
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+    private ListPreference mListPreference;
+    SharedPreferences sharedPreferences;
 
 
     @Override
@@ -17,6 +26,55 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefshow);
 
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            initSummary(getPreferenceScreen().getPreference(i));
+        }
+
+
     }
 
+
+    private void initSummary(android.preference.Preference p) {
+        if (p instanceof PreferenceCategory) {
+            PreferenceCategory pCat = (PreferenceCategory) p;
+            for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                initSummary(pCat.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
+    }
+
+    private void updatePrefSummary(android.preference.Preference p) {
+        if (p instanceof android.preference.ListPreference) {
+            android.preference.ListPreference listPref = (android.preference.ListPreference) p;
+            p.setSummary(listPref.getEntry());
+            p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("language", "").commit();
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePrefSummary(findPreference(key));
+    }
 }
